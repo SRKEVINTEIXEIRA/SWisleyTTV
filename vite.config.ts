3a47eee8380f375076454233ddf68d8e12d43004
Contador Kevin Teixeira <kevinpaladinoteixeira@gmail.com>
2025-06-18 00:30:17 -0300

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react({
+      // opcional: força o JSX clássico
+      // jsxRuntime: 'classic'
+    })
+  ],
   base: '/SWisleyTTV/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react']
-  }
-});
+})
