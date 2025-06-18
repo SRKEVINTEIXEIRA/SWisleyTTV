f985e60757b657d25f46513d6f6177fe24b70c49
Contador Kevin Teixeira <kevinpaladinoteixeira@gmail.com>
2025-06-18 01:44:45 -0300

    feat: Configura deploy automático com GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SWisleyTTV/'     // <-- cuidado com a barra inicial e final
+  base: '/SWisleyTTV/', // <-- ADICIONE ESTA LINHA
 })
